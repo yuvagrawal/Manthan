ee0dfcc7c87ed2c9dbfbc69574cbd1551441abce
Yuvraj <yuvrajagrawal61@gmail.com>
2025-04-15 19:53:14 +0530

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoute from  "./routes/userRouter.js";
+import messageRoute from "./routes/messageRouter.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import {app,server} from "./socket/socket.js"
+
+dotenv.config({});
+
+//const app = express();
+
+
+//middlewares
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+    origin: (origin, callback) => {
+      callback(null, origin); 
+    },
+    credentials: true
+  };
+  
+app.use(cors(corsOptions));
+
+
+//routes
+app.use("/api/v1/user",userRoute);
+app.use("/api/v1/message",messageRoute);
+
+
+const PORT = process.env.PORT;
+server.listen(PORT,()=>{
+    connectDB();
+    console.log(`server listening at port ${PORT}`)
+});
