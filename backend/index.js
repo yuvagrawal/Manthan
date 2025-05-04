eba417a709320b7b9d1d8f77ddd50c091ce1d21a
Yuvraj <yuvrajagrawal61@gmail.com>
2025-05-04 22:36:30 +0530

    commit after build

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ import userRoute from  "./routes/userRouter.js";
 import messageRoute from "./routes/messageRouter.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {app,server} from "./socket/socket.js"
+import {app,server} from "./socket/socket.js";
+import path from "path" ;
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({});
 
@@ -13,6 +17,7 @@ dotenv.config({});
 
 
 //middlewares
+app.use(express.static(path.resolve(__dirname,'build')));
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(cookieParser());
